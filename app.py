1e64ba836f6c0ca31f49dc22cda52c12eb9cc133
hang-1n-there <s22037@gsm.hs.kr>
2023-10-30 14:56:56 +0900

    🎨 train.py $ app.py 구조 개선

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ from PIL import Image
 import base64
 import time
 
-import config
-import train
+from config import cfg
+from train import model
+from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 app = Flask(__name__)
 
 # AI 모델 판단 후 결과 응답
@@ -35,8 +36,26 @@ def img_to_result(image):
     image = np.asarray(image)
     image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
     
+    model.cfg = cfg
+
+    result = inference_detector(model, image)
     
-    return
+    # 예측 클래스와 확률(또는 점수)을 저장할 변수
+    max_score = -1  # 가장 높은 점수
+    best_class = None  # 가장 높은 점수를 갖는 클래스
+
+    # 결과를 반복하며 가장 높은 점수를 갖는 클래스 찾기
+    for bbox in result:
+        if bbox.shape[0] != 0:
+            # 클래스 정보는 0번째 열에, 해당 클래스의 점수는 1번째 열에 있다고 가정합니다.
+            class_score = bbox[0, 4]  # 예측된 객체의 클래스 점수
+
+            # 현재 클래스의 점수가 최고 점수보다 높으면 갱신
+            if class_score > max_score:
+                max_score = class_score
+                best_class = int(bbox[0, 0])  # 해당 클래스
+
+    return best_class
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0' ,port = 8001, debug=True)
--- train.py
+++ train.py
@@ -8,10 +8,15 @@ import os.path as osp
 
 from config import cfg
 
-datasets = [build_dataset(cfg.data.train)]
-model = build_detector(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
-model.CLASSES = datasets[0].CLASSES
+def train():
+    datasets = [build_dataset(cfg.data.train)]
+    model = build_detector(cfg.model, train_cfg=cfg.get('train_cfg'), test_cfg=cfg.get('test_cfg'))
+    model.CLASSES = datasets[0].CLASSES
 
-mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
-# epochs는 config의 runner 파라미터로 지정됨. 기본 12회
-train_detector(model, datasets, cfg, distributed=False, validate=True)
+    mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
+    # epochs는 config의 runner 파라미터로 지정됨. 기본 12회
+    train_detector(model, datasets, cfg, distributed=False, validate=True)
+
+
+if __name__ == '__main__':
+    train()
