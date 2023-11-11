6946357786da6b9ca29145435d53f8418de9e4dd
hang-1n-there <s22037@gsm.hs.kr>
2023-11-11 17:04:17 +0900

    :sparkles: image_split.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from io import BytesIO
 from PIL import Image
-import base64
+import requests
 import cv2
 import numpy as np
 
@@ -11,44 +11,40 @@ from mmdet.apis import inference_detector, init_detector
 app = Flask(__name__)
 
 # Load the model and configurations
-model = init_detector(config='path_to_config_file', checkpoint='path_to_checkpoint_file', device='cuda:0')
+model = init_detector(config='mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py', checkpoint='mmdetection/checkpoints/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth', device='cuda:1')
 
-# AI 모델 판단 후 결과 응답
-@app.route("/model", methods=['POST'])
-def decision():
-    if request.method == 'POST':
-        # Get the base64-encoded image from the JSON request with key 'id'
-        params = request.get_json()['id']
-        
-        # Convert the base64 encoded image to PIL Image
-        img = Image.open(BytesIO(base64.b64decode(params)))
-        result = img_to_result(img)
-        return str(result)
-    else:
-        return 'Backend-server Connect'
-
-def img_to_result(image):
+# Function to process image from URL and get the best class
+def url_to_best_class(image_url):
+    response = requests.get(image_url)
+    image = Image.open(BytesIO(response.content))
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
     result = inference_detector(model, image)
     
-    # Variables to store predicted class and probability (or score)
-    max_score = -1  # Highest score
-    best_class = None  # Class with the highest score
+    max_score = -1
+    best_class = None
 
-    # Iterate through the results to find the class with the highest score
     for bbox in result:
         if bbox.shape[0] != 0:
-            # Assuming class information is in the 0th column and the score of that class is in the 1st column
-            class_score = bbox[0, 4]  # Score of the predicted object class
-
-            # If the score for the current class is higher than the highest score, update it
+            class_score = bbox[0, 4]
             if class_score > max_score:
                 max_score = class_score
-                best_class = int(bbox[0, 0])  # The corresponding class
+                best_class = int(bbox[0, 0])
 
     return best_class
+
+# Route to accept image URL and process it
+@app.route("/process_image", methods=['POST'])
+def process_image():
+    if request.method == 'POST':
+        # Get the image URL from the JSON request with key 'image_url'
+        image_url = request.get_json()['image_url']
+        
+        best_class = url_to_best_class(image_url)
+        return jsonify({'best_class': best_class})
+    else:
+        return 'Backend-server Connect'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001, debug=True)
---
+++ image_split.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+
+def split_data():
+
+    # 원본 데이터셋 폴더와 저장할 폴더 지정
+    original_dataset_folder = 'C:/cv_project\Recycling_trash/Separate_Collection/trash_dataset'
+    train_folder = 'C:/cv_project/Recycling_trash/Separate_Collection/train'
+
+    # train 폴더가 없으면 생성
+    if not os.path.exists(train_folder):
+        os.makedirs(train_folder)
+
+    # 각 batch 폴더를 돌면서 파일을 읽어와 train 폴더에 저장
+    start_index = 0
+    for batch_folder in range(1, 45):
+        batch_folder_name = f'batch_{batch_folder}'
+        batch_folder_path = os.path.join(original_dataset_folder, batch_folder_name)
+
+        # batch 폴더가 존재하는지 확인
+        if os.path.exists(batch_folder_path):
+            # batch 폴더 안에 있는 파일들을 정렬
+            file_list = sorted(os.listdir(batch_folder_path))
+            
+            # train 폴더에 파일을 복사하면서 이름을 변경
+            for i, file_name in enumerate(file_list):
+                source_path = os.path.join(batch_folder_path, file_name)
+                destination_path = os.path.join(train_folder, f'{start_index + i}.jpg')
+                shutil.copyfile(source_path, destination_path)
+            
+            # 다음 batch 폴더에서 시작할 인덱스 업데이트
+            start_index += len(file_list)
+
+print("파일 복사가 완료되었습니다.")
+
+
+if __name__ == '__main__':
+    split_data()
